de35ddf67430e05711387982434735133b6de96c
DESKTOP-QUST204\nqwrt <nqwrt@ymail.com>
2022-04-15 23:53:10 +0900

    Initial commit

---
+++ src/main/java/edu/hi/zenkins/ZenkinsApplication.java
@@ -0,0 +1,13 @@
+package edu.hi.zenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hi/zenkins/ZenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hi.zenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
